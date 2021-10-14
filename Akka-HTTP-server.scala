85641e749802799686bcb9213efb3fe52f097ffa
tayssirmlaiki <83220448+tayssirmlaiki@users.noreply.github.com>
2021-10-14 23:52:06 +0200

    Update Akka-HTTP-server.scala

--- Akka-HTTP-server.scala
+++ Akka-HTTP-server.scala
@@ -1,1 +1,27 @@
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
 
+object WebServer extends App {
+		
+
+    implicit val actorSystem = akka.actor.ActorSystem("messaging-actorsystem")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = actorSystem.dispatcher
+		
+    
+    def helloRoute: Route = pathEndOrSingleSlash {
+        complete("Welcome to messaging service")
+    }
+		
+  
+    val binding = Http().bindAndHandle(helloRoute, "localhost", 8080)
+    println("Server running...")
+		
+   
+    StdIn.readLine()
+    binding.flatMap(_.unbind()).onComplete(_ => actorSystem.terminate())
+    println("Server is shut down")
+}
