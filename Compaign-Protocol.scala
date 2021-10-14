1d399ebda451c55c46f21ac968c413911e349af2
tayssirmlaiki <83220448+tayssirmlaiki@users.noreply.github.com>
2021-10-14 19:29:23 +0200

    Update Compaign-Protocol.scala

--- Compaign-Protocol.scala
+++ Compaign-Protocol.scala
@@ -1,1 +1,3 @@
-
+case class Compaign( id: Int, country: String, targeting: Targeting, banners: List[Banner], bid: Double)
+case class Targeting( targetedSiteIds: Array[])
+case class Banner( id: Int, src: String, width: Int, height: Int)
