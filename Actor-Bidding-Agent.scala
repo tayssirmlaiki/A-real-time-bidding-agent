5c42f8c0e32ac1ffdc706bbe77fe2e25cff5cc7b
tayssirmlaiki <83220448+tayssirmlaiki@users.noreply.github.com>
2021-10-14 22:33:11 +0200

    Create Actor-Bidding-Agent.scala

---
+++ Actor-Bidding-Agent.scala
@@ -0,0 +1,19 @@
+package multithreading
+import akka.actor.ActorSystem
+import akka.actor.Actor
+import akka.actor.Props
+
+object ActorBiddingAgent extends App{
+  class ActorAgent extends Actor{
+    def receive={
+      case s:String => println("The "+s)
+      case i:Int => println(" in "+i)
+    }
+    def foo = println("Normal Method")
+  }
+  val system =ActorSystem ("ActorAgent")
+  val actor = system.actorOf(Props[ActorAgent],"ActorAgent")
+  
+  actor! "Bidding Agent"
+  actor! 2021
+}
